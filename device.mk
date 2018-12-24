6b86ec09ec358584f8dbd6f2a6104b3531a65053
Pranav Vashi <neobuddy89@gmail.com>
2018-12-24 16:17:54 +0530

    angler: Add vndk package and libs
    Change-Id: I937cd398c8cf781bc915c80f18b03f114cf23726
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- device.mk
+++ device.mk
@@ -633,6 +633,17 @@ PRODUCT_PACKAGES += \
     gatekeeper.msm8994 \
     keystore.msm8994
 
+# VNDK
+PRODUCT_PACKAGES += \
+    vndk_package
+
+PRODUCT_COPY_FILES += \
+    prebuilts/vndk/v27/arm64/arch-arm64-armv8-a/shared/vndk-core/android.hardware.gnss@1.0.so:$(TARGET_COPY_OUT_VENDOR)/lib64/android.hardware.gnss@1.0-v27.so
+
+# VNDK-SP
+PRODUCT_PACKAGES += \
+    vndk-sp
+
 # b/29995499
 $(call add-product-sanitizer-module-config,cameraserver,never)
 $(call add-product-sanitizer-module-config,mm-qcamera-daemon,never)
